1869a455b150d5ad201ef4a1c08567e3e472e3ac
Miha Meglic <miha@meglic.dev>
2023-10-01 23:27:05 +0200

    Prepared for movie content, style changes

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,6 +10,32 @@ const newsCollection = defineCollection({
   }),
 });
 
+const filmCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string().nonempty(),
+    year: z.number(),
+    cover: z.string().url(),
+    poster: z.string().url(),
+    // thumbnail: z.string().url(),
+    genre: z.array(z.string()),
+    runtime: z.string(),
+    aspectRatio: z.string(),
+    color: z.enum(['color', 'black and white']),
+    soundFormat: z.string(),
+    imdbId: z
+      .string()
+      .regex(/^tt\d+$/)
+      .optional(),
+    trailer: z.string().optional(), // youtube id
+    teasers: z.array(z.string()).optional(), // youtube ids
+    stills: z.array(z.string().url()).optional(),
+    awards: z.array(z.string()).optional(),
+    pressPack: z.string().url().optional(),
+  }),
+});
+
 export const collections = {
+  films: filmCollection,
   news: newsCollection,
 };
