de9393b49021705823a980b7d16d46b9f0105569
Miha Meglic <miha@meglic.dev>
2023-10-02 20:08:26 +0200

    Style changes to master layout, moved film pages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			margin: {
+				'1/3': '33.333333%',
+				'1/4': '25%',
+			}
+		},
 	},
 	plugins: [],
 };
