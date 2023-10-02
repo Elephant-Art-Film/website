28cd123918a0e081eaf8e2f4c82084789c4ec859
Miha Meglic <miha@meglic.dev>
2023-10-02 20:57:16 +0200

    Added Montserrat font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
@@ -6,6 +8,9 @@ module.exports = {
 			margin: {
 				'1/3': '33.333333%',
 				'1/4': '25%',
+			},
+			fontFamily: {
+				'montserrat': ['Montserrat', ...defaultTheme.fontFamily.sans],
 			}
 		},
 	},
