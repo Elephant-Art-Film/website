cc518046bd6597d793029fdc7c9c328ffdc04712
Miha Meglic <miha@meglic.dev>
2023-10-02 22:48:03 +0200

    Added mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://elephant.meglic.dev',
-	integrations: [tailwind()],
+  site: 'https://elephant.meglic.dev',
+  integrations: [tailwind(), mdx()]
 });
