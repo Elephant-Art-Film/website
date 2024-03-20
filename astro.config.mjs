5fc5eac73a1d05bd077fb3e3955cf18d3848443b
Miha Meglic <miha@meglic.dev>
2024-03-20 19:00:00 +0100

    Remove Vue
    Currently unused, no sense in having it installed.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from "@astrojs/mdx";
 
-import vue from "@astrojs/vue";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://elephant-art-film.com',
-  integrations: [tailwind(), mdx(), vue()]
+  integrations: [tailwind(), mdx()]
 });
